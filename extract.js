32e63d47ba2a96ed4c5f5dbcd42b7df0d46c3397
Nicolas Levrard <nicolaslevrard@gmail.com>
2022-11-30 18:56:17 +0100

    add comments

--- extract.js
+++ extract.js
@@ -7,59 +7,50 @@ async function main(){
 
     const uri = DB_URI;
  
-    const joi_id = 'joi-2022'
+    const joi_id = 'joi-2022';
 
     const client = new MongoClient(uri);
 
     const db = client.db("forumorg-prod");
 
-    const activities = {}
+    const activities = {};
 
-    const activities_titles = {}
+    const activities_titles = {};
  
     try {
-        // Connect to the MongoDB cluster
+        // connection au cluster
         await client.connect();
- 
-        const joi = await db.collection("events").findOne( {'id': 'joi-2022'} )
-        joi.round_tables.forEach( (round_table) => {
-            slot_dict = {}
-            for (slot in round_table.slots) {
-                slot_dict[slot] = []
-            }
-            //console.log(round_table.id)
-            //console.log(round_table.id)
-            activities[round_table.id] = slot_dict 
-            activities_titles[round_table.id] = round_table.title
-        })
-
-        joi.conferences.forEach( (conference) => {
-            slot_dict = {}
-            for (slot in conference.slots) {
-                slot_dict[slot] = []
-            }
-            //console.log(round_table.id)
-            //console.log(round_table.id)
-            activities[conference.id] = slot_dict 
-            activities_titles[conference.id] = conference.title
-        })
-        //console.log(activities)
         
+        // initialisation d'un dictionnaire type { activity_id: { slot_id: [student #1, student #2] } }
+        const joi = await db.collection("events").findOne( {'id': 'joi-2022'} );
+        joi.round_tables.forEach( (round_table) => { // pour chaque table ronde
+            slot_dict = {};
+            for (slot in round_table.slots) {
+                slot_dict[slot] = [];
+            }
+            activities[round_table.id] = slot_dict;
+            activities_titles[round_table.id] = round_table.title; // dictionnaire liant l'id de l'activité à son titre
+        })
+        joi.conferences.forEach( (conference) => { // pour chaque conférence
+            slot_dict = {};
+            for (slot in conference.slots) {
+                slot_dict[slot] = [];
+            }
+            activities[conference.id] = slot_dict;
+            activities_titles[conference.id] = conference.title; // dictionnaire liant l'id de l'activité à son titre
+        })
+        
+        // recupération des étudiants inscrits à l'événement et ajout dans le dictionnaire
         await db.collection("users").find( {'events.joi-2022.slots': {$exists: true}} )
         .forEach( user => {
             for (slot in user.events[joi_id].slots) {
-                //console.log(slot)
-                //console.log(user.events[joi_id].slots[slot])
                 //if (user.events[joi_id].slots[slot] in ('k9avr', 'gFeuu', '0PPv3', 'ce3UP', 'YgLau', 'lsXwb', 'eAZAp', 'rfWTz', '36VsS', 'wAqBY', 'nUCfG', 'MYRIR', 'XFNDe', '6uDOG')) {
                     activities[ user.events[joi_id].slots[slot] ][ slot ].push([user.profile.first_name, user.profile.name, user.id])
                 //}
             }
         })
 
-        //console.log(activities['ce3UP']['9h45 - 10h30'])
-
-        //console.log(activities)
-
+        // création du fichier excel
         for (activity_id in activities) {
             for (slot_id in activities[activity_id]) {
                 /* écrire un CSV
@@ -74,14 +65,14 @@ async function main(){
                 */
 
                 // écrire un XLSX
-                const title = [activities_titles[activity_id]]
-                const slot = [slot_id]
-                const header = ['first_name', 'name', 'email']
-                const data = activities[activity_id][slot_id]
-                data.unshift(header)
-                data.unshift([''])
-                data.unshift(slot)
-                data.unshift(title)
+                const title = [activities_titles[activity_id]];
+                const slot = [slot_id];
+                const header = ['first_name', 'name', 'email'];
+                const data = activities[activity_id][slot_id];
+                data.unshift(header);
+                data.unshift(['']);
+                data.unshift(slot);
+                data.unshift(title);
 
 
                 const merge1 = XLSX.utils.decode_range("A1:C1");
@@ -97,7 +88,8 @@ async function main(){
                 XLSX.writeFile(workBook, './output/'+activity_id + '_' + slot_id.split(' - ')[0] + '_' + slot_id.split(' - ')[1]+".xlsx");
             }
         }
-        console.log("done")
+
+        console.log("done");
 
     } catch (e) {
         console.error(e);
